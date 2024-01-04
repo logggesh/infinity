296911e52ee3faa2322032657957c23c754de769
logggesh <123285648+logggesh@users.noreply.github.com>
2024-01-04 12:34:27 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -6,8 +6,10 @@
     <title>Infinity</title>
     <link rel="stylesheet" href="index.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+    <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
 </head>
-<body class="bg-dark">
+<body style="background: linear-gradient(to right,#005067,#b2b2b2,#00667b);">
 <nav class="navbar navbar-expand-lg bg-body-black">
   <div class="container-fluid">
   <a class="navbar-brand text-white ms-3" href="#">
@@ -51,9 +53,23 @@
     <div class="col-lg-12 d-flex justify-content-center align-items-center mt-5">
       <div class="abt-us-cont">
         <div class="row">
-          <div class="col-lg-12">
-            <h4>About us</h4>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam consequuntur sapiente, vel laborum quod repudiandae est ex ea molestias vero. Inventore eos necessitatibus dicta laborum numquam praesentium blanditiis amet impedit?</p>
+          <div class="col-lg-6">
+          <div class="abt-us-disp justify-content-center d-flex align-items-center">
+            <lottie-player src="https://lottie.host/0803472b-88c4-446b-892d-83752ffb5e3d/jRiM1ZXCfX.json" background="##FFFFFF" speed="1" style="width: 500px; height: 500px" loop autoplay direction="1" mode="normal"></lottie-player>
+          </div>
+          </div>
+          <div class="col-lg-6 mt-2">
+            <h1 class="heading-color">ABOUT US</h1>
+            <p class="content mt-4">
+            <b>"Dive into the Digital Cosmos"</b>
+<br><br>
+An digital delight to all the  enthusiastic technocrats out there with our grand symposium!!!
+<br><br>
+Infinity 2k24 is back with it's majestic cosmic fourth edition of the intra-college technical and non-technical symposium. A new fusion of fun and technical wisdom emphasizes the digital world of advanced technologies in identifying new interests despite the challenging need to develop ,learn and contribute more to the technical age with our five captivating technical events, three amazing non-technical events and a flagship event.
+<br><br>
+<b>Open for 1st, 2nd & 3rd years of all 
+B.E/B.Tech/B.Sc/M.Sc/MCA programes.</b>
+            </p>
           </div>
         </div>
       </div>
